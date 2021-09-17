94bfe9f6352909c17f66c88a63add81dec41c412
gtsimis <gt2186@gmail.com>
2021-09-17 14:24:39 -0400

    Added cat views and index method

--- 99cats/ninetyninecats/config/routes.rb
+++ 99cats/ninetyninecats/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+    # get 'cats/index'
+    # get 'cats/new'
+    # get 'cats/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cats
 end
--- 99cats/ninetyninecats/db/seeds.rb
+++ 99cats/ninetyninecats/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Cat.create(name: 'Joey', birthdate: '2018-10-04', color: 'Tabby', sex: 'M', description: 'He is the best cat ever!' )
+Cat.create(name: 'Elliot', birthdate: '1992-06-14', color: 'Calico', sex: 'M', description: 'He knows what Calico means!' )
+Cat.create(name: 'George', birthdate: '1986-08-21', color: 'Orange', sex: 'M', description: 'He doesn\'t know what Calico is!')
+Cat.create(name: 'Anne', birthdate: '2016-09-24', color: 'Black', sex: 'F', description: 'He is the best cat ever!' )
+Cat.create(name: 'Chloe', birthdate: '2017-11-07', color: 'White', sex: 'F', description: 'He is the best cat ever!' )
